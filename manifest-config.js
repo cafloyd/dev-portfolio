2df155b530e6142383f95b9793679ac7cfa78c64
Caitlin Floyd <caitlinfloyd+coding@gmail.com>
2021-01-21 09:25:00 -0500

    style: update manifest and favicons

--- manifest-config.js
+++ manifest-config.js
@@ -1,8 +1,8 @@
 const colors = require('./colors');
 
 module.exports = {
-  name: 'Mate Gatsby Starter',
-  short_name: 'Mate Gatsby Starter',
+  name: 'Caitlin FLoyd',
+  short_name: 'Caitlin Floyd',
   start_url: '/',
   background_color: colors.background,
   theme_color: colors.primary,
