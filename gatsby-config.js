c0dbc83f5021971b05a5a728a51356c00794e432
Caitlin Floyd <caitlinfloyd+coding@gmail.com>
2022-01-16 19:53:03 -0500

    chore: configure google analytics settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ const contentful = require('contentful');
 const manifestConfig = require('./manifest-config');
 require('dotenv').config();
 
-const { ACCESS_TOKEN, SPACE_ID, ANALYTICS_ID, DETERMINISTIC } = process.env;
+const { ACCESS_TOKEN, SPACE_ID, DETERMINISTIC } = process.env;
 
 const client = contentful.createClient({
   space: SPACE_ID,
@@ -41,42 +41,18 @@ const plugins = [
 module.exports = client.getEntries().then((entries) => {
   const { mediumUser } = entries.items.find(getAboutEntry).fields;
 
-  plugins.push(
-    {
-      resolve: 'gatsby-source-medium',
-      options: {
-        username: mediumUser || '@medium',
-      },
+  plugins.push({
+    resolve: 'gatsby-source-medium',
+    options: {
+      username: mediumUser || '@medium',
     },
-    // {
-    //   resolve: `gatsby-plugin-google-analytics`,
-    //   options: {
-    //     // The property ID; the tracking code won't be generated without it
-    //     trackingId: 'G-L4N8WYC5DJ',
-    //     // Defines where to place the tracking script - `true` in the head and `false` in the body
-    //     head: false,
-    //     // Setting this parameter is optional
-    //     anonymize: true,
-    //     // Setting this parameter is also optional
-    //     respectDNT: true,
-    //     // Delays sending pageview hits on route update (in milliseconds)
-    //     pageTransitionDelay: 0,
-    //     // Defers execution of google analytics script after page load
-    //     defer: false,
-    //     // defaults to false
-    //     enableWebVitalsTracking: true,
-    //   },
-    // },
-  );
-
-  if (ANALYTICS_ID) {
-    plugins.push({
-      resolve: 'gatsby-plugin-google-analytics',
-      options: {
-        trackingId: 'G-L4N8WYC5DJ',
-      },
-    });
-  }
+  });
+  plugins.push({
+    resolve: 'gatsby-plugin-google-analytics',
+    options: {
+      trackingId: 'G-L4N8WYC5DJ',
+    },
+  });
 
   return {
     siteMetadata: {
