a3c80523ea335778a25ee5f57e0909fc74c31496
Caitlin Floyd <caitlinfloyd+coding@gmail.com>
2022-03-09 20:09:08 -0500

    feature: Add Resources page redirect

---
+++ src/pages/resources.js
@@ -0,0 +1,6 @@
+const ResourcesPage = () =>
+  window.location.replace(
+    'https://caitlinfloyd.notion.site/Tips-Resources-for-Early-Career-Devs-3132541c8a594708837e5ba7ae7251a5',
+  );
+
+export default ResourcesPage;
