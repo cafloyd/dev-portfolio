7a32c6c7e22aa4136446f3916944259c4c187fda
Caitlin Floyd <caitlinfloyd+coding@gmail.com>
2022-03-09 22:33:51 -0500

    style: Style 404 page, add footer

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import FontAwesomeIcon from 'react-fontawesome';
 import { Heading, Box, Image } from 'rebass/styled-components';
 import styled from 'styled-components';
 import Layout from '../components/Layout';
 import Section from '../components/Section';
 import Triangle from '../components/Triangle';
 import Tanuki from '../components/Logo/Tanuki.png';
+import '../../static/font-awesome-custom/ig.css';
 
 const Background = () => (
   <div>
@@ -30,18 +32,25 @@ const Background = () => (
     />
   </div>
 );
+const ERROR_SM = '300px';
+const ERROR_MD = '400px';
+const ERROR_LG = '450px';
 
 const ErrorImage = styled(Image)`
   border-radius: 50%;
   padding: 40px;
-  margin-top: 0px;
+  margin: 0 auto;
 `;
 
 const NotFoundPage = () => (
   <Layout>
-    <Section.Container id="404" Background={Background}>
-      <Box width={[320, 400, 600]} m="auto">
-        <Heading color="primary" fontSize={['3rem', '4rem', '5rem']} as="h1">
+    <Section.Container
+      class="notFoundContainer"
+      id="404"
+      Background={Background}
+    >
+      <Box width={[ERROR_SM, ERROR_MD, ERROR_LG]} m="auto">
+        <Heading color="primary" fontSize={['3rem', '4rem', '4rem']} as="h1">
           404
         </Heading>
         <Heading
@@ -52,12 +61,46 @@ const NotFoundPage = () => (
           I couldn&apos;t find what you were looking for, but I did find a cute
           puppy!
         </Heading>
-        <a href="https://www.instagram.com/princess_tanuki/">
+        <a href="https://www.instagram.com/princess_tanuki/" margin="auto">
           <ErrorImage
+            width={[ERROR_SM, ERROR_MD, ERROR_LG]}
+            height={[ERROR_SM, ERROR_MD, ERROR_LG]}
             src={Tanuki}
             alt="A coonhound mix puppy looking at you lovingly"
           />
         </a>
+      </Box>
+      <Box className="footer">
+        <button className="footerButton" type="button">
+          <a
+            href="https://www.linkedin.com/in/caitlinfloyd/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="socialLink"
+          >
+            <FontAwesomeIcon name="fab fa-linkedin" />
+          </a>
+        </button>
+        <button className="footerButton" type="button">
+          <a
+            href="https://github.com/cafloyd"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="socialLink"
+          >
+            <FontAwesomeIcon name="fab fa-github" />
+          </a>
+        </button>
+        <button className="footerButton" type="button">
+          <a
+            href="https://caitlinfloyd.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="socialLink"
+          >
+            <FontAwesomeIcon name="fab fa-globe" />
+          </a>
+        </button>
       </Box>
     </Section.Container>
   </Layout>
