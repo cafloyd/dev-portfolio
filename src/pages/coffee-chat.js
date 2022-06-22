b82444ca99025a5754d042723e80fab98962d238
Caitlin Floyd <caitlinfloyd+coding@gmail.com>
2022-06-21 22:42:55 -0400

    Feat: Add coffee chat form

---
+++ src/pages/coffee-chat.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Widget } from '@typeform/embed-react';
+
+const CoffeeChatPage = () => (
+  <Widget
+    id="v21CXF4t"
+    style={{
+      width: '100%',
+      height: 'calc(100vh - 16px)',
+      margin: '0',
+      padding: '0',
+    }}
+    className="my-form"
+  />
+);
+
+export default CoffeeChatPage;
